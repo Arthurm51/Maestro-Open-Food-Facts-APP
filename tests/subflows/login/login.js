1c14a8f6aed61a6373febc4f6c9ecc0a1c1ce608
Arthur Machado <thuur51@gmail.com>
2025-02-11 23:30:36 -0300

    Feature: login

---
+++ tests/subflows/login/login.js
@@ -0,0 +1,8 @@
+output.login = {
+    data: {
+        userName: "automationtest01arthur",
+        email: "automacao1teste@gmail.com",
+        name: "Teste Automacao",
+        password: 123456,
+    }
+};
